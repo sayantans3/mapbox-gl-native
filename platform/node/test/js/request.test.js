29d92523893578286c79d474ffe11d5ad7c47a9d
John Firebaugh <john.firebaugh@gmail.com>
2017-07-19 15:30:29 -0700

    [node] Reset Style::Impl::lastError when loading a new style

--- platform/node/test/js/request.test.js
+++ platform/node/test/js/request.test.js
@@ -81,3 +81,29 @@ test(`render reports an error if the request function throws an exception`, func
         t.end();
     });
 });
+
+test(`render does not report an error from rendering a previous style`, function(t) {
+    var map = new mbgl.Map({
+        request: function(req, callback) {
+            var data = mockfs.dataForRequest(req);
+            if (mockfs.source_vector === data) {
+                callback(new Error('message'));
+            } else {
+                callback(null, { data: data });
+            }
+        }
+    });
+    map.load(mockfs.style_vector);
+    map.render({ zoom: 16 }, function(err, pixels) {
+        t.assert(err);
+        t.assert(/message/.test(err.message));
+        t.assert(!pixels);
+
+        map.load(mockfs.style_raster);
+        map.render({ zoom: 16 }, function(err, pixels) {
+            t.error(err);
+            t.assert(pixels);
+            t.end();
+        });
+    });
+});
